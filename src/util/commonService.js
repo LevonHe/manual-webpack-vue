611d9b0b8a949f569bdc97731cc2119a8509b33a
Levon.he <Levon.he@fatritech.com>
2020-04-03 09:36:10 +0800

    feat: 添加公共方法和正则

--- src/util/commonService.js
+++ src/util/commonService.js
@@ -10,3 +10,273 @@ export const formatRequestParams = (params) => {
   });
   return params;
 };
+// 获取url参数
+export const getUrlParam = (name) => {
+  let url = location.href;
+  let temp = url.split('?');
+  let paramStr = temp[1];
+  let param = paramStr.split('&');
+  let obj = {};
+  for (let i = 0; i < param.length; i += 1) {
+    let item = param[i].split('=');
+    let key = item[0];
+    let value = item[1];
+    obj[key] = value;
+  }
+  return obj[name];
+};
+// 获取页面横向滚动条未知
+export const getPageScrollLeft = () => {
+  return document.documentElement.scrollLeft || document.body.scrollLeft || window.pageXOffset;
+};
+// 获取页面纵向滚动条未知
+export const getPageScrollTop = () => {
+  return document.documentElement.scrollTop || document.body.scrollTop || window.pageYOffset;
+};
+// 获取某个dom元素的offset和scroll距离
+export const getRootOffsetAndScroll = (el, pname = 'body') => {
+  // 对于要计算偏移和滚动长度的父元素，要设置相对定位
+  let offsetLeft = el.offsetLeft;
+  let offsetTop = el.offsetTop;
+  let scrollLeft = el.scrollLeft;
+  let scrollTop = el.scrollTop;
+  if (el.offsetParent && el.offsetParent.tagName !== pname) {
+    let p = getRootOffsetAndScroll(el.offsetParent, pname);
+    offsetLeft += p[0];
+    offsetTop += p[1];
+    scrollLeft += p[2];
+    scrollTop += p[3];
+  }
+  return [offsetLeft, offsetTop, scrollLeft, scrollTop];
+};
+// 开启全屏
+export const launchFullScreen = (element) => {
+  if (element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if (element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if (element.msRequestFullscreen) {
+    element.msRequestFullscreen();
+  } else if (element.webkitRequestFullscreen) {
+    element.webkitRequestFullScreen();
+  }
+};
+// 关闭全屏
+export const exitFullscreen = () => {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  }
+};
+// 返回顶部
+export const scrollToTop = () => {
+  const top = document.documentElement.scrollTop || document.body.scrollTop || window.pageYOffset;
+  if (top > 0) {
+    window.requestAnimationFrame(scrollToTop);
+    window.scrollTo(0, top - top / 8);
+  }
+};
+
+// 金额格式化，三位加逗号
+export const formatMoney = (num) => {
+  num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+};
+
+// 截取字符串，加省略号
+export const subText = (str, length) => {
+  if (str.length === 0) {
+    return '';
+  }
+  if (str.length > length) {
+    return str.substr(0, length) + '...';
+  } else {
+    return str;
+  }
+};
+
+// base64转file
+export const base64ToFile = (base64, filename) => {
+  let arr = base64.split(',');
+  let mime = arr[0].match(/:(.*?);/)[1];
+  let suffix = mime.split('/')[1];
+  let bstr = atob(arr[1]);
+  let n = bstr.length;
+  let u8arr = new Uint8Array(n);
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n);
+  }
+  return new File([u8arr], `${filename}.${suffix}`, { type: mime });
+};
+// base64转blob
+export const base64ToBlob = (base64) => {
+  let arr = base64.split(',');
+  let mime = arr[0].match(/:(.*?);/)[1];
+  let bstr = atob(arr[1]);
+  let n = bstr.length;
+  let u8arr = new Uint8Array(n);
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n);
+  }
+  return new Blob([u8arr], { type: mime });
+};
+
+// blob转file
+export const blobToFile = (blob, filename) => {
+  blob.lastModifiedDate = new Date();
+  blob.name = filename;
+  return blob;
+};
+
+// file转base64
+export const fileToBase64 = (file) => {
+  let reader = new FileReader();
+  reader.readAsDataURL(file);
+  reader.onload = function(e) {
+    return e.target.result;
+  };
+};
+
+// pc or 移动设备
+export const detectDeviceType = () => {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ? 'Mobile' : 'PC';
+};
+
+// 判断手机类型
+export const getOSType = () => {
+  let u = navigator.userAgent;
+  // let app = navigator.appVersion;
+  let isAndroid = u.indexOf('Android') > -1 || u.indexOf('Linux') > -1;
+  let isIOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
+  if (isIOS) {
+    return 0;
+  }
+  if (isAndroid) {
+    return 1;
+  }
+  return 2;
+};
+
+// 判断数据类型
+export const type = (target) => {
+  let ret = typeof target;
+  let obj = {
+    '[object Array]': 'array',
+    '[object Object]': 'object',
+    '[object Number]': 'number-object',
+    '[object Boolean]': 'boolean-object',
+    '[object String]': 'string-object',
+  };
+
+  if (target === null) {
+    return 'null';
+  } else if (ret === 'object') {
+    let str = Object.prototype.toString.call(target);
+    return obj[str];
+  } else {
+    return ret;
+  }
+};
+// 生成指定范围随机数
+export const randomNum = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+// 数组中某个元素的出现次数
+export const eleOccurCount = (arr, el) => {
+  return arr.reduce((a, v) => (v === el ? a + 1 : a + 0), 0);
+};
+// 去除空格
+/**
+ * @param {string} str
+ * @param {number} type 1-所有空格 2-前后空格 3-前空格 4-后空格
+ */
+export const trim = (str, type = 1) => {
+  if (type && type !== 1 && type !== 2 && type !== 3 && type !== 4) {
+    return;
+  }
+  switch (type) {
+    case 1:
+      return str.replace(/\s/g, '');
+    case 2:
+      return str.replace(/(^\s)|(\s*$)/g, '');
+    case 3:
+      return str.replace(/(^\s)/g, '');
+    case 4:
+      return str.replace(/(\s$)/g, '');
+    default:
+      return str;
+  }
+};
+// html转义
+export const escapeHTML = (str) => {
+  str.replace(
+    /[&<>'"]/g,
+    (tag) => ({ '&': '&amp;', '<': '&lt;', '>': '&gt;', "'": '&#39;', '"': '&quot;' }[tag] || tag)
+  );
+};
+// 加
+export const add = (num1, num2) => {
+  let n1, n2, m;
+  try {
+    n1 = num1.toString().split('.')[1].length;
+  } catch (error) {
+    n1 = 0;
+  }
+  try {
+    n2 = num2.toString().split('.')[1].length;
+  } catch (error) {
+    n2 = 0;
+  }
+  m = Math.pow(10, Math.max(n1, n2));
+  return (num1 * m + num2 * m) / m;
+};
+// 减
+export const sub = (num1, num2) => {
+  let n1, n2, m, n;
+  try {
+    n1 = num1.toString().split('.')[1].length;
+  } catch (error) {
+    n1 = 0;
+  }
+  try {
+    n2 = num2.toString().split('.')[1].length;
+  } catch (error) {
+    n2 = 0;
+  }
+  m = Math.pow(10, Math.max(n1, n2));
+  n = n1 >= n2 ? n1 : n2;
+  return Number(((num1 * m - num2 * m) / m).toFixed(n));
+};
+// 乘
+export const multiplication = (num1, num2) => {
+  let m = 0,
+    n1 = num1.toString(),
+    n2 = num2.toString();
+  try {
+    m += n1.split('.')[1].length;
+  } catch (error) {}
+  try {
+    m += n2.split('.')[1].length;
+  } catch (error) {}
+
+  return (Number(n1.replace('.', '')) * Number(n2.replace('.', ''))) / Math.pow(10, m);
+};
+// 除
+export const division = (num1, num2) => {
+  let r1, r2, n1, n2;
+  try {
+    n1 = num1.toString().split('.')[1].length;
+  } catch (error) {
+    n1 = 0;
+  }
+  try {
+    n2 = num2.toString().split('.')[1].length;
+  } catch (error) {
+    n2 = 0;
+  }
+  r1 = Number(num1.toString().replace('.', ''));
+  r2 = Number(num2.toString().replace('.', ''));
+  return (r1 / r2) * Math.pow(10, n2 - n1);
+};
--- src/util/regExpService.js
+++ src/util/regExpService.js
@@ -1,38 +1,74 @@
 // 登录账号：邮箱/手机号码
 export const AccountReg = /^((\w-*\.*)+@(\w-?)+(\.\w{2,})+)|([1][3,4,5,7,8,9][0-9]{9})$/;
-// 邮箱
-export const EmailReg = /^(\w-*\.*)+@(\w-?)+(\.\w{2,})+$/;
-// 手机号码
-export const TelephoneReg = /^[1][3,4,5,7,8,9][0-9]{9}$/;
 // 密码：8～20位，必须同时包含字母、数字及除去回车、空格、TAB的特殊符号
 export const PasswordReg = /^(?=.*\d)(?=.*[A-Za-z])(?!.*\s)(?=.*[^A-Za-z0-9]+)\S*$/;
+// 手机号，严格
+export const phoneStrict = /^(?:(?:\+|00)86)?1(?:(?:3[\d])|(?:4[5-7|9])|(?:5[0-3|5-9])|(?:6[5-7])|(?:7[0-8])|(?:8[\d])|(?:9[1|8|9]))\d{8}$/;
+// 手机号，宽松
+export const phoneRelax = /^(?:(?:\+|00)86)?1[3-9]\d{9}$/;
+// 座机
+export const landline = /\d{3}-\d{8}|\d{4}-\d{7}/;
+// 邮箱
+export const email = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 // 经度：取值范围0-180,保留0-6位小数
 export const LongitudeReg = /^[+-]?(((\d|[1-9]\d|1[0-7]\d|0{1,3})\.\d{0,6})|(\d|[1-9]\d|1[0-7]\d|0{1,3})|180\.0{0,6}|180)$/;
 // 纬度：取值范围0-90,保留0-6位小数
 export const LatitudeReg = /^[+-]?([0-8]?\d{1}\.\d{0,6}|90\.0{0,6}|[0-8]?\d{1}|90)$/;
-// url：以http开头
-export const HttpUrlReg = /^http:\/\/[A-Za-z0-9]+\.[A-Za-z0-9]+[\\/=\\?%\-&_~`@[\]\\':+!]*([^<>\\"\\"])*$/;
-// 版本号：格式如a.b.c，a、b、c由最大4位数字组合
-export const VersionReg = /^[0-9]+\.\d+\.\d+$/; // 版本号：格式如a.b.c，a、b、c由最大4位数字组合
-// 标识符：1～32位，字符：英文、"_-"、数字、允许中间有空格
-export const IdentifierReg = /^[A-Za-z0-9_-](([A-Za-z0-9_-\s]{0,30}[A-Za-z0-9_-])|([A-Za-z0-9_-]{0,31}))$/;
-// 通用：允许中间有空格
-export const TrimReg = /^\S((.{0,253}\S)|(\S{0,254}))$/;
-// 用户名：8～32位，字符：由8~32位英文字母、数字、@、点、下划线、中划线(除回车、TAB外)组成
-export const UserNameReg = /^\S((.{6,30}\S)|(\S{7,31}))$/;
-// 名称：1～32位，字符：由1~32位英文字母、数字、@、点、下划线、中划线(除回车、TAB外)组成
-export const Name32Reg = /^\S((.{0,30}\S)|(\S{0,31}))$/;
-// 名称：1～64位，字符：由1~64位英文字母、数字、@、点、下划线、中划线(除回车、TAB外)组成
-export const Name64Reg = /^\S((.{0,62}\S)|(\S{0,63}))$/;
-// 设备序列号：1～32位，英文、数字、中划线、下划线、点、@
-export const DeviceSerialNumberReg = /^[a-zA-Z0-9.-_@]{1,32}$/;
 // 正整数
 export const positiveInteger = /^\d+$/;
 // 非负浮点数
 export const positiveFloat = /^\d+(\.\d+)?$/;
 // 浮点数
 export const Float = /^-?\d+(\.\d+)?$/;
-// 工况名称：1~32位，字符：由1~32位英文字母、数字、@、点、下划线、中划线(除回车、TAB外)组成
-export const ConditionNameReg = /^\S((.{0,30}\S)|(\S{0,31}))$/;
-// 传动比
-export const RatioReg = /^\d+(\.\d+)?[:：]\d+(\.\d+)?$/;
+// 不能包含字母
+export const noWord = /^[^A-Za-z]*$/;
+// 中国邮政编码
+export const postCode = /^(0[1-7]|1[0-356]|2[0-7]|3[0-6]|4[0-7]|5[1-7]|6[1-7]|7[0-5]|8[013-6])\d{4}$/;
+// 微信号：6-20位，以字母开头，由字母、数字、中划线、下划线组成
+export const weChatNumber = /^[a-zA-Z][-_a-zA-Z0-9]{5,19}$/;
+// 16进制颜色
+export const color16 = /^#?([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/;
+// 火车车次
+export const trainNumber = /^[GCDZTSPKXLY1-9]\d{1,4}$/;
+// 手机机身码
+export const IMEI = /^\d{15,17}$/;
+// 必须带端口的url
+export const httpAndPort = /^((ht|f)tps?:\/\/)?[\w-]+(\.[\w-]+)+:\d{1,5}\/?$/;
+// 网址，支持端口和query参数
+export const website = /^((ht|f)tps?):\/\/?[\w-]+(\.[\w-]+)+([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?$/;
+// 统一社会信用代码
+export const creditCode = /^[0-9A-HJ-NPQRTUWXY]{2}\d{6}[0-9A-HJ-NPQRTUWXY]{10}$/;
+// 迅雷链接
+export const thunderLink = /^thunderx?:\/\/[a-zA-Z\d]+=$/;
+// ed2k链接
+export const ed2kLink = /^ed2k:\/\/\|file\|.+\|\/$/;
+// 磁力链接
+export const magnetLink = /^magnet:\?xt=urn:btih:[0-9a-fA-F]{40,}.*$/;
+// 子网掩码
+export const subnetMask = /^(?:\d{1,2}|1\d\d|2[0-4]\d|25[0-5])(?:\.(?:\d{1,2}|1\d\d|2[0-4]\d|25[0-5])){3}$/;
+// linux文件夹路径
+export const linuxFolderPath = /^(\/[^/]+)+\/?$/;
+// linux文件夹路径
+export const linuxFilePath = /^(\/[^/]+)+$/;
+// windows文件夹路径
+export const windowsFolderPath = /^[a-zA-Z]:\\(?:\w+\\?)*$/;
+// windows文件夹路径
+export const windowsFilePath = /^[a-zA-Z]:\\(?:\w+\\)*\w+\.\w+$/;
+// 股票代码（A股）
+export const ashare = /(s[hz]|S[HZ])(000[\d]{3}|002[\d]{3}|300[\d]{3}|600[\d]{3}|60[\d]{4})$/;
+// X.Y.Z
+export const version3 = /^\d+(?:\.\d+){2}$/;
+// 视频链接地址
+export const videoUrl = /^https?:\/\/(.+\/)+.+(\.(swf|avi|flv|mpg|rm|mov|wav|asf|3gp|mkv|rmvb|mp4))$/;
+// 图片链接地址
+export const imageUrl = /^https?:\/\/(.+\/)+.+(\.(gif|png|jpg|jpeg|webp|svg|psd|bmp|tif))$/;
+// 银行卡号
+export const accountNumber = /^[1-9]\d{9,29}$/;
+// 中文姓名
+export const chineseName = /^(?:[\u4e00-\u9fa5·]{2,16})$/;
+// 英文姓名
+export const englishName = /^[a-zA-Z]{1}[a-zA-Z\s]{0,20}[a-zA-Z]{1}$/;
+// 车牌号（新能源+非新能源）
+export const licensePlateNumberNER = /^(?:[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领 A-Z]{1}[A-HJ-NP-Z]{1}(?:(?:[0-9]{5}[DF])|(?:[DF](?:[A-HJ-NP-Z0-9])[0-9]{4})))|(?:[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领 A-Z]{1}[A-Z]{1}[A-HJ-NP-Z0-9]{4}[A-HJ-NP-Z0-9 挂学警港澳]{1})$/;
+// 身份证号码(1代和2代)
+export const idCard = /(^\d{8}(0\d|10|11|12)([0-2]\d|30|31)\d{3}$)|(^\d{6}(18|19|20)\d{2}(0\d|10|11|12)([0-2]\d|30|31)\d{3}(\d|X|x)$)/;
